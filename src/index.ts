d50c9bcf7ced7480930965503582db4de3e79e49
alexandre.vuillermot <alexandre.vuillermot@kuehne-nagel.com>
2020-11-24 23:00:26 +0100

    manage jwt token

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express = require('express');
 import cors = require('cors')
 import url = require('url');
+import moment = require('moment');
+import { v4 as uuidv4 } from 'uuid';
+import jwt from 'jsonwebtoken';
 import { ApplicationDbTestSettings as DbSettings, ApplicationDbSettings } from './config/config';
 import bodyParser = require('body-parser');
 import ServiceUser from '../src/controllers/security/user.controller'
@@ -20,8 +23,10 @@ app.put('/logon', async (req, res) => {
     let servUser: ServiceUser = new ServiceUser();
     
     try {
-        let back:any = await servUser.logon(req.body.login, req.body.password);
-        res.send(back);
+        let back: { login: string, email: string } = await servUser.logon(req.body.login, req.body.password);
+        let body: any = { id: uuidv4(), created: moment.utc(), login: back.login, entities:[], email: back.email, expire: moment.utc().add(8, "hours").toDate(), type: "USER", credentials: [] };
+        let encrypt: string = await jwt.sign(body, 'PERRIGNY21160');
+        res.send({ token: encrypt });
     }
     catch (ex) {
         res.status(401).send();
